70aeb6003faaad431e9e7a44037e1dcf4dcd3067
Clinton Mzee <clinton.mzee@outlook.com>
2023-06-17 15:03:02 +0200

    add fonts

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -1,18 +1,18 @@
-import { useState } from "react";
-
+import React, { useState } from "react";
+import './Clocks.css';
 
 function StartButton( props ) {
     // button with start
 
-    return <button onClick={ props.function }>  Start </button>
+    return <button className="primary" onClick={ props.function }>  Start </button>
 }
 
 function PauseButton(props) {
-    return <button onClick={ props.function }>  Pause </button>
+    return <button className="primary" onClick={ props.function }>  Pause </button>
 }
 
 function ResetButton(props) {
-    return <button onClick={ props.function }>  Reset </button>
+    return <button className="secondary" onClick={ props.function }>  Reset </button>
 }
 
 
@@ -35,21 +35,31 @@ const ResetClock = () => {
 
 function Clock() {
     
-    const {flag, setFlag} = useState(true)
-    const {seconds, setSeconds} = useState(43);
-    const {minutes, setMinutes} = useState(0);
-    const {hours, setHours} = useState(400);
+    const [ flag, setFlag ] = useState(true);
+    const [ hours, setHours ] = useState(0);
+    const [ minutes, setMinutes ] = useState(0);
+    const [ seconds, setSeconds ] = useState(0);
+    const [ milliseconds, setMilliseconds ] = useState(0);
+
+    
+
+    function Add() {
+        setInterval(setHours( hours + 1 ), 100)
+    }
 
     return (
         <div>
             <h1> React {hours} JS Stopwatch </h1>
-
-            <p> {hours} : {minutes} : {seconds} </p>
-            { flag ? <StartButton function={StartClock} /> : <PauseButton function={PauseClock} />}
+            { window.hours = hours }
+            <p> {hours} : {minutes} : {seconds} : {milliseconds}</p>
+             <StartButton function={Add} /> 
+              <PauseButton function={PauseClock} />
             <ResetButton function={ResetClock} />
         </div>
 
     )
 }
+
+console.log(window.hours)
 
 export {Clock}
